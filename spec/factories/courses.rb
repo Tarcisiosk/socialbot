aad2fce4728c2de93d88289026a68ac8c5beb718
tarcisiosk <tarcisioskora@hotmail.com>
2018-02-08 22:58:19 -0200

    adjust course factory to support new fields

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :course do
-    url 'whatisgoingon.com'
-    name 'Awesome Course'
-    description 'a mind blowing course about life and universe'
+    title 'Awesome Course'
+    description = 'a mind blowing course about life and universe'
+    time_available = "2018-02-08T23:43:44.698Z".to_time
+    logo_url = 'whatisgoingon.com'
   end
 end
